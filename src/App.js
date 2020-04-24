b69a6af5bde4f6269cb6cac24eb830b7c1cf62a6
Alec Leamas <leamas.alec@nowhere.net>
2020-04-24 23:33:54 +0200

    Add licensing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 //https://www.gnu.org/software/librejs/free-your-javascript.html
 //
 //https://github.com/react-bootstrap/react-bootstrap/blob/master/LICENSE
+
+/*
+ * ***************************************************************************
+ *   Copyright (C) 2020 Alec Leamas                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+
+*/
 import React from 'react';
 import { Card, Button, Form} from 'react-bootstrap';
 
